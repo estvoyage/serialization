7cb17ab094150de03b0faccf0ab6eeed7ebf6b9b
Frédéric Hardy <frederic.hardy@mageekbox.net>
2015-03-14 21:23:44 +0100

    Add type to argument.

--- classes/serializer/csv.php
+++ classes/serializer/csv.php
@@ -63,22 +63,22 @@ final class csv implements serialization\serializer, data\provider
 
 	function stringObjectPropertyHasValue(object\property $property, object\property\string $string)
 	{
-		return $this->addPropertyWithValue($property, $string);
+		return $this->addPropertyWithValue($property, new data\data((string) $string));
 	}
 
 	function integerObjectPropertyHasValue(object\property $property, object\property\integer $integer)
 	{
-		return $this->addPropertyWithValue($property, $integer);
+		return $this->addPropertyWithValue($property, new data\data((string) $integer));
 	}
 
 	function floatObjectPropertyHasValue(object\property $property, object\property\float $float)
 	{
-		return $this->addPropertyWithValue($property, $float);
+		return $this->addPropertyWithValue($property, new data\data((string) $float));
 	}
 
 	function booleanObjectPropertyHasValue(object\property $property, object\property\boolean $boolean)
 	{
-		return $this->addPropertyWithValue($property, $boolean->value ? 1 : 0);
+		return $this->addPropertyWithValue($property, new data\data($boolean->value ? '1' : '0'));
 	}
 
 	function storableObjectPropertyHasValue(object\property $property, object\storable $storable)
@@ -99,13 +99,13 @@ final class csv implements serialization\serializer, data\provider
 
 	function nullObjectProperty(object\property $property)
 	{
-		return $this->addPropertyWithValue($property, '');
+		return $this->addPropertyWithValue($property, new data\data(''));
 	}
 
-	private function addPropertyWithValue(object\property $property, $value)
+	private function addPropertyWithValue(object\property $property, data\data $value)
 	{
 		$this->header[] = new data\data((! $this->namespace ? '' : join('.', $this->namespace) . '.') . $property);
-		$this->line[] = new data\data((string) $value);
+		$this->line[] = $value;
 
 		return $this;
 	}
